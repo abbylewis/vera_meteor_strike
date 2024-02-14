cd971ef4878a876277e1bbaee5dcb945e3355c58
abbylewis <aslewis@vt.edu>
2024-02-14 09:01:00 -0500

    script to load target variables from google sheets

---
+++ load_target_variables.R
@@ -0,0 +1,8 @@
+#Load from google sheets
+library(googlesheets4)
+target_vars <- read_sheet('https://docs.google.com/spreadsheets/d/1fOWo6zlcWA8F6PmRS9AD6n1pf-dTWSsmGKNpaX3yHNE/edit#gid=0')
+priority_daily <- target_vars |>
+  dplyr::filter(duration == "P1D") |>
+  dplyr::filter(`priority target` == T)
+
+write.csv(priority_daily, "priority_daily.csv", row.names = F)
